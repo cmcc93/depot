d2e2cbcd7ecd84706e96b46396e42a8047c6c13d
Casey McCourt <cas.mcco@gmail.com>
2013-02-26 01:00:01 -0800

    carts woohooo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
+
+  resources :line_items
+
+  resources :carts
 
 
   # The priority is based upon order of creation:
@@ -52,6 +58,8 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+
+  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
